f7662278cfe4206d18e637935babb7571ed48420
Hariz Norizan <--get>
2022-09-25 16:43:15 +0800

    Fix vercel build error:
    Conflicting library between @vue/compiler-sfc & vite/plugin-vue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [vue()],
     test: {
         globals: true,
-        environment: 'happy-dom',
-        setupFiles: '/tests/pokemonResponseConfig.js'
+        environment: 'happy-dom'
     }
 })
